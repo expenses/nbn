d057d3be0f52af8334549462285ca80cefc2e401
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-10-15 21:42:53 +0200

    Fix shaders

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,3 +1,3 @@
 rm ./shaders/compiled/*
-slang-gen-ninja ./shaders/* -I FidelityFX-SDK/sdk/include/FidelityFX/gpu  --build-dir ./shaders/compiled --output ./build.ninja -- -O3 -warnings-as-errors all
+slang-gen-ninja ./shaders/*  --build-dir ./shaders/compiled --output ./build.ninja -- -O3 -warnings-as-errors all
 ninja
