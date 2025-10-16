046035ca8a5745495b3d7ceb5632fc0ec8fb2115
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-10-16 18:26:17 +0200

    Tidy up window example

--- examples/window.rs
+++ examples/window.rs
@@ -47,12 +47,14 @@ impl winit::application::ApplicationHandler for App {
             .unwrap();
         let device = nbn::Device::new(Some(&window));
 
-        let swapchain =
-            device.create_swapchain(&window, vk::ImageUsageFlags::COLOR_ATTACHMENT, nbn::SurfaceSelectionCriteria {
+        let swapchain = device.create_swapchain(
+            &window,
+            vk::ImageUsageFlags::COLOR_ATTACHMENT,
+            nbn::SurfaceSelectionCriteria {
                 write_via_compute: false,
-                desire_hdr: false
-
-            });
+                desire_hdr: false,
+            },
+        );
         let shader = device.load_reloadable_shader("shaders/compiled/triangle.spv");
         let pipeline = create_pipeline(&device, &shader, &swapchain);
 
@@ -77,119 +79,121 @@ impl winit::application::ApplicationHandler for App {
         _window_id: winit::window::WindowId,
         event: winit::event::WindowEvent,
     ) {
+        let (state, device) = if let Some((state, device)) = self
+            .window_state
+            .as_mut()
+            .and_then(|state| self.device.as_ref().map(|device| (state, device)))
+        {
+            (state, device)
+        } else {
+            return;
+        };
+
         match event {
             winit::event::WindowEvent::Resized(new_size) => {
-                let window_state = self.window_state.as_mut().unwrap();
-
-                window_state.swapchain.create_info.image_extent = vk::Extent2D {
+                state.swapchain.create_info.image_extent = vk::Extent2D {
                     width: new_size.width,
                     height: new_size.height,
                 };
-                let device = self.device.as_ref().unwrap();
                 unsafe { device.queue_wait_idle(*device.graphics_queue).unwrap() };
-                device.recreate_swapchain(&mut window_state.swapchain);
+                device.recreate_swapchain(&mut state.swapchain);
             }
             winit::event::WindowEvent::RedrawRequested => {
-                let device = self.device.as_ref().unwrap();
+                if state.shader.try_reload(device) {
+                    unsafe { device.queue_wait_idle(*device.graphics_queue).unwrap() };
+                    state.pipeline = create_pipeline(device, &state.shader, &state.swapchain);
+                }
 
-                if let Some(state) = self.window_state.as_mut() {
-                    if state.shader.try_reload(device) {
-                        unsafe { device.queue_wait_idle(*device.graphics_queue).unwrap() };
-                        state.pipeline = create_pipeline(device, &state.shader, &state.swapchain);
-                    }
+                unsafe {
+                    let command_buffer =
+                        &state.per_frame_command_buffers[state.sync_resources.current_frame];
+                    let mut frame = state.sync_resources.wait_for_frame(device);
 
-                    unsafe {
-                        let command_buffer =
-                            &state.per_frame_command_buffers[state.sync_resources.current_frame];
-                        let mut frame = state.sync_resources.wait_for_frame(device);
+                    let (next_image, _suboptimal) = device
+                        .swapchain_loader
+                        .acquire_next_image(
+                            *state.swapchain,
+                            !0,
+                            *frame.image_available_semaphore,
+                            vk::Fence::null(),
+                        )
+                        .unwrap();
+                    let image = &state.swapchain.images[next_image as usize];
 
-                        let (next_image, _suboptimal) = device
-                            .swapchain_loader
-                            .acquire_next_image(
-                                *state.swapchain,
-                                !0,
-                                *frame.image_available_semaphore,
-                                vk::Fence::null(),
-                            )
-                            .unwrap();
-                        let image = &state.swapchain.images[next_image as usize];
-
-                        device.reset_command_buffer(command_buffer);
-                        device
-                            .begin_command_buffer(
-                                **command_buffer,
-                                &vk::CommandBufferBeginInfo::default(),
-                            )
-                            .unwrap();
-                        device.cmd_pipeline_barrier2(
+                    device.reset_command_buffer(command_buffer);
+                    device
+                        .begin_command_buffer(
                             **command_buffer,
-                            &vk::DependencyInfo::default().image_memory_barriers(&[
-                                nbn::NewImageBarrier {
-                                    image,
-                                    src: Some(nbn::BarrierOp::Acquire),
-                                    dst: nbn::BarrierOp::ColorAttachmentWrite,
-                                    src_queue_family_index: command_buffer.queue_family_index,
-                                    dst_queue_family_index: command_buffer.queue_family_index,
-                                }
-                                .into(),
-                            ]),
-                        );
+                            &vk::CommandBufferBeginInfo::default(),
+                        )
+                        .unwrap();
+                    device.cmd_pipeline_barrier2(
+                        **command_buffer,
+                        &vk::DependencyInfo::default().image_memory_barriers(&[
+                            nbn::NewImageBarrier {
+                                image,
+                                src: Some(nbn::BarrierOp::Acquire),
+                                dst: nbn::BarrierOp::ColorAttachmentWrite,
+                                src_queue_family_index: command_buffer.queue_family_index,
+                                dst_queue_family_index: command_buffer.queue_family_index,
+                            }
+                            .into(),
+                        ]),
+                    );
 
-                        device.begin_rendering(
-                            command_buffer,
-                            state.swapchain.create_info.image_extent.width,
-                            state.swapchain.create_info.image_extent.height,
-                            &[vk::RenderingAttachmentInfo::default()
-                                .image_view(*image.view)
-                                .image_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
-                                .clear_value(vk::ClearValue {
-                                    color: vk::ClearColorValue { float32: [1.0; 4] },
-                                })
-                                .load_op(vk::AttachmentLoadOp::CLEAR)
-                                .store_op(vk::AttachmentStoreOp::STORE)],
-                            None,
-                        );
-                        device.cmd_bind_pipeline(
-                            **command_buffer,
-                            vk::PipelineBindPoint::GRAPHICS,
-                            *state.pipeline,
-                        );
-                        device.cmd_draw(**command_buffer, 3, 1, 0, 0);
+                    device.begin_rendering(
+                        command_buffer,
+                        state.swapchain.create_info.image_extent.width,
+                        state.swapchain.create_info.image_extent.height,
+                        &[vk::RenderingAttachmentInfo::default()
+                            .image_view(*image.view)
+                            .image_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                            .clear_value(vk::ClearValue {
+                                color: vk::ClearColorValue { float32: [0.0; 4] },
+                            })
+                            .load_op(vk::AttachmentLoadOp::CLEAR)
+                            .store_op(vk::AttachmentStoreOp::STORE)],
+                        None,
+                    );
+                    device.cmd_bind_pipeline(
+                        **command_buffer,
+                        vk::PipelineBindPoint::GRAPHICS,
+                        *state.pipeline,
+                    );
+                    device.cmd_draw(**command_buffer, 3, 1, 0, 0);
 
-                        device.cmd_end_rendering(**command_buffer);
-                        device.cmd_pipeline_barrier2(
-                            **command_buffer,
-                            &vk::DependencyInfo::default().image_memory_barriers(&[
-                                nbn::NewImageBarrier {
-                                    image,
-                                    src: Some(nbn::BarrierOp::ColorAttachmentWrite),
-                                    dst: nbn::BarrierOp::Present,
-                                    src_queue_family_index: command_buffer.queue_family_index,
-                                    dst_queue_family_index: command_buffer.queue_family_index,
-                                }
-                                .into(),
-                            ]),
-                        );
+                    device.cmd_end_rendering(**command_buffer);
+                    device.cmd_pipeline_barrier2(
+                        **command_buffer,
+                        &vk::DependencyInfo::default().image_memory_barriers(&[
+                            nbn::NewImageBarrier {
+                                image,
+                                src: Some(nbn::BarrierOp::ColorAttachmentWrite),
+                                dst: nbn::BarrierOp::Present,
+                                src_queue_family_index: command_buffer.queue_family_index,
+                                dst_queue_family_index: command_buffer.queue_family_index,
+                            }
+                            .into(),
+                        ]),
+                    );
 
-                        device.end_command_buffer(**command_buffer).unwrap();
+                    device.end_command_buffer(**command_buffer).unwrap();
 
-                        frame.submit(
-                            device,
-                            &image,
-                            &[vk::CommandBufferSubmitInfo::default()
-                                .command_buffer(**command_buffer)],
-                        );
-                        device
-                            .swapchain_loader
-                            .queue_present(
-                                *device.graphics_queue,
-                                &vk::PresentInfoKHR::default()
-                                    .wait_semaphores(&[*image.render_finished_semaphore])
-                                    .swapchains(&[*state.swapchain])
-                                    .image_indices(&[next_image]),
-                            )
-                            .unwrap();
-                    }
+                    frame.submit(
+                        device,
+                        &image,
+                        &[vk::CommandBufferSubmitInfo::default().command_buffer(**command_buffer)],
+                    );
+                    device
+                        .swapchain_loader
+                        .queue_present(
+                            *device.graphics_queue,
+                            &vk::PresentInfoKHR::default()
+                                .wait_semaphores(&[*image.render_finished_semaphore])
+                                .swapchains(&[*state.swapchain])
+                                .image_indices(&[next_image]),
+                        )
+                        .unwrap();
                 }
             }
             winit::event::WindowEvent::KeyboardInput {
