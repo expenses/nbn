8a6a4876853785e2649f7b9c8478301fd85049ce
Ashley Ruglys <ashley.ruglys@gmail.com>
2026-04-21 19:30:07 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -77,7 +77,7 @@
               '')
             ];
             buildInputs = [vulkan-loader];
-            LD_LIBRARY_PATH = lib.makeLibraryPath [wayland libxkbcommon libxcursor libx11 libxi];
+            LD_LIBRARY_PATH = lib.makeLibraryPath [vulkan-loader wayland libxkbcommon libxcursor libx11 libxi];
           };
         devShells.slang-build = with pkgs;
           mkShell {
