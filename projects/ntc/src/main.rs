de6f3bcc5fe3bf315a67a1f6b8f0ab3db75bb926
Ashley Ruglys <ashley.ruglys@gmail.com>
2026-05-13 15:05:27 +0200

    Initial implementation of writer struct

--- projects/ntc/src/main.rs
+++ projects/ntc/src/main.rs
@@ -68,6 +68,7 @@ struct TextureData {
     size: u32,
     num_mip_levels: i32,
     num_blocks: u32,
+    block_offsets: Vec<u32>,
     bitmasks: nbn::Buffer,
 }
 
@@ -81,11 +82,14 @@ impl TextureData {
         let mut size_in_blocks = size / 4;
         let mut num_blocks = size_in_blocks * size_in_blocks;
 
+        let mut block_offsets = vec![0];
+
         let mut num_mip_levels = 1;
         while size_in_blocks > 1 {
             num_mip_levels += 1;
             size_in_blocks >>= 1;
-            num_blocks += size_in_blocks * size_in_blocks
+            block_offsets.push(num_blocks);
+            num_blocks += size_in_blocks * size_in_blocks;
         }
 
         let num_values = num_blocks as usize * (3 * 2 + 16);
@@ -105,6 +109,7 @@ impl TextureData {
                     .collect::<Vec<_>>(),
             ),
             bitmasks,
+            block_offsets,
             num_blocks,
             size,
             num_mip_levels,
@@ -573,4 +578,114 @@ fn l1_psnr(loss: f32) -> f32 {
 
 fn l2_psnr(loss: f32) -> f32 {
     10.0 * (1.0 / loss).log10()
+}
+
+struct AntcTexture<'a> {
+    size: u32,
+    block_offsets: &'a [u32],
+    bytes: &'a [u8],
+}
+
+impl<'a> AntcWriter<'a> {
+    fn write<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        writer.write_all(b"ANTC")?;
+        writer.write_all(&0_u32.to_le_bytes())?;
+
+        let mut offset = 4
+            + 4
+            + 4
+            + self
+                .textures
+                .iter()
+                .map(|tex| 4 + 4 + tex.block_offsets.len() as u32 * 4)
+                .sum::<u32>();
+
+        writer.write_all(&offset.to_le_bytes())?;
+
+        offset += self.weights.len() as u32;
+
+        for texture in &self.textures {
+            writer.write_all(&texture.size.to_le_bytes())?;
+            writer.write_all(&(texture.block_offsets.len() as u32).to_le_bytes())?;
+        }
+
+        for texture in &self.textures {
+            for block_offset in texture.block_offsets {
+                writer.write_all(&(offset + block_offset).to_le_bytes())?;
+            }
+            offset += texture.bytes.len() as u32;
+        }
+
+        writer.write_all(&self.weights)?;
+        for texture in &self.textures {
+            writer.write_all(&texture.bytes)?;
+        }
+
+        Ok(())
+    }
+}
+
+struct AntcWriter<'a> {
+    weights: &'a [u8],
+    textures: [AntcTexture<'a>; 4],
+}
+
+#[test]
+fn text_writer() {
+    let writer = AntcWriter {
+        weights: &[1, 3, 5, 7, 9],
+        textures: [
+            AntcTexture {
+                size: 2,
+                block_offsets: &[0, 4],
+                bytes: &[0, 1, 2, 3, 5],
+            },
+            AntcTexture {
+                size: 2,
+                block_offsets: &[0, 8],
+                bytes: nbn::cast_slice(&[6, 7, 8, 9, 77_u16]),
+            },
+            AntcTexture {
+                size: 1,
+                block_offsets: &[0],
+                bytes: &[33],
+            },
+            AntcTexture {
+                size: 1,
+                block_offsets: &[0],
+                bytes: &[44],
+            },
+        ],
+    };
+
+    let mut output = Vec::new();
+
+    writer.write(&mut output).unwrap();
+
+    assert_eq!(&output[..4], b"ANTC");
+
+    let words: &[u32] = nbn::cast_slice(&output[4..12]);
+
+    let version = words[0];
+    assert_eq!(version, 0);
+
+    let weight_offset = words[1] as usize;
+
+    assert_eq!(&output[weight_offset..weight_offset + 5], &[1, 3, 5, 7, 9]);
+
+    let texture_info: &[[u32; 2]] = nbn::cast_slice(&output[12..12 + 4 * (4 + 4)]);
+
+    assert_eq!(texture_info, &[[2; 2], [2; 2], [1; 2], [1; 2]]);
+
+    let mip_offsets: &[u32] =
+        nbn::cast_slice(&output[12 + 4 * (4 + 4) + (2 * 4)..12 + 4 * (4 + 4) + (2 * 4) + (2 * 4)]);
+
+    assert_eq!(
+        &nbn::cast_slice::<_, u16>(&output[mip_offsets[0] as usize..mip_offsets[0] as usize + 8]),
+        &[6, 7, 8, 9]
+    );
+    assert_eq!(
+        &nbn::cast_slice::<_, u16>(&output[mip_offsets[1] as usize..mip_offsets[1] as usize + 2]),
+        &[77]
+    );
 }
