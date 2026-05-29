52ca0d61a1d11e81f283ef8a133351515967932c
Ashley Ruglys <ashley.ruglys@gmail.com>
2026-05-29 20:28:40 +0200

    add blender export script

---
+++ tools/blender_export_instances.py
@@ -0,0 +1,37 @@
+import bpy
+import json
+import numpy as np
+
+obj = bpy.context.active_object
+
+depsgraph = bpy.context.evaluated_depsgraph_get()
+
+m = {}
+total = 0
+
+last = None
+
+for inst in depsgraph.object_instances:
+    if not inst.is_instance:
+        continue
+
+    name = inst.object.name
+    
+    if inst.parent.name != last:
+        print(inst.parent.name)
+        last = inst.parent.name
+
+    if not name in m:
+        m[name] = []
+    
+    # Copy is required otherwise the same matrix just gets duplicated
+    m[name].append(inst.matrix_world.copy())
+    total += 1
+
+np_data = {
+    name: np.array(matrices, dtype=np.float32).ravel()
+    for name, matrices in m.items()
+}
+
+np.savez_compressed("out.npz", **np_data)
+print(total)
